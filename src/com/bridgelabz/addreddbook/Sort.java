cfd741c4b478ad7fe85f3a2ffce55c6fb182caf8
mudable <mudableshilpa@gmail.com>
2021-11-21 11:53:41 +0530

    Added : UC11 to sort the name

--- src/com/bridgelabz/addreddbook/Sort.java
+++ src/com/bridgelabz/addreddbook/Sort.java
@@ -3,14 +3,20 @@ package com.bridgelabz.addreddbook;
 import java.util.Comparator;
 
 public class Sort {
-	static Comparator<Contacts> compareCity = new Comparator<Contacts>() {
-		public int compare(Contacts one, Contacts two) {
-			return one.getCity().compareTo(two.getCity());
-		}
-	};
-	static Comparator<Contacts> compareState = new Comparator<Contacts>() {
-		public int compare(Contacts one, Contacts two) {
-			return one.getState().compareTo(two.getState());
-		}
-	};
+    static Comparator<Contacts> compareCity = new Comparator<>() {
+        public int compare(Contacts one, Contacts two) {
+            return one.getCity().compareTo(two.getCity());
+        }
+    };
+    static Comparator<Contacts> compareState = new Comparator<>() {
+        public int compare(Contacts one, Contacts two) {
+
+            return one.getState().compareTo(two.getState());
+        }
+    };
+    static Comparator<Contacts> compareFirstName = new Comparator<>() {
+        public int compare(Contacts one, Contacts two) {
+            return one.getFirstName().compareTo(two.getFirstName());
+        }
+    };
 }
